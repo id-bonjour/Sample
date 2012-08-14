7fde477b60f9636c32d9595ba3855b0c65beadaa
id-bonjour <id.ikeda@gmail.com>
2012-08-15 00:50:01 +0800

    revert commit

--- ConsoleApplication1/ConsoleApplication1/Class1.cs
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -7,5 +7,11 @@ namespace ConsoleApplication1
 {
     class Class1
     {
+<<<<<<< HEAD
+        //add comments
+
+        //add commit2
+=======
+>>>>>>> parent of 8a27d33... local commit
     }
 }
